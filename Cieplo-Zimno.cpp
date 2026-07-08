fe003775d9ac262ab3642a53c9dc1e9aa415c04c
Aurelian <AurelianKowalski@gmail.com>
2026-07-08 15:51:39 +0200

    Latest version.2

--- Cieplo-Zimno.cpp
+++ Cieplo-Zimno.cpp
@@ -1,33 +1,63 @@
 #include <iostream>
 #include <random>
+//komunikat jezeli uzytkownik nie poda nic
+//usun niepotrzebne pliki z repozytorium
+
 
 int secretNumber;
-int guess = 0;
+float guess = 0;
 char playAgain = 'y';
 bool hasGuess = false;
+bool isCharacter = false;
 
 void randomizer() {
      // Step 1: Setup random number generator
-    std::random_device rd;   // Non-deterministic seed
-    std::mt19937 gen(rd());  // Mersenne Twister engine
-    std::uniform_int_distribution<> dist(1, 100); // Range [1, 100]
+     std::random_device rd;   // Non-deterministic seed
+     std::mt19937 gen(rd());  // Mersenne Twister engine
+     std::uniform_int_distribution<> dist(1, 100); // Range [1, 100]
 
-    secretNumber = dist(gen);
+     secretNumber = dist(gen);
 }
 
 void guessing() {
-std::cout << "Guess the number between 1 and 100! ";
-     std::cin >> guess;
+        // If user enters nothing and presses enter, input extraction fails and loop continues
+          while (true) {
+          std::cout << "Guess the number between 1 and 100! ";
+          if (!(std::cin >> guess)) {
+          std::cin.clear();
+          std::cin.ignore(10000, '\n');
+          } 
+          else {
+          isCharacter = false;
+          break;
+          }
+        }
 }
 
 void toLow() {
-     std::cout << "Too low! Try again! ";
-     std::cin >> guess;
+        std::cout << "Too low! Try again! ";
+        // If user enters nothing and presses enter, input extraction fails
+        if (!(std::cin >> guess)) {
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        isCharacter = true;
+        } 
+        else {
+        isCharacter = false;
+     }
 }
 
 void toHigh() {
      std::cout << "Too high! Try again! ";
-     std::cin >> guess;
+     // If user enters nothing and presses enter, input extraction fails
+     if (!(std::cin >> guess)) {
+     std::cin.clear();
+     std::cin.ignore(10000, '\n');
+     isCharacter = true;
+     std::cout << "Brutha, do you understand was is a number?\n";
+     } else {
+     isCharacter = false;
+     }
 }
 
 void pinPoint() {
@@ -35,10 +65,10 @@ void pinPoint() {
      std::cout << "Wow you hit it! Would you like to play again(Y/N)? ";
      std::cin >> playAgain;
      if(playAgain == 'Y' || playAgain == 'y') {
-guessing();
+     guessing(); 
      }
      else {
-          std::cout << "Fah you for leaving me!!!";
+     std::cout << "Fah you for leaving me!!!";
      }
 }
 
@@ -46,20 +76,23 @@ int main() {
      randomizer();
 
      while (playAgain == 'Y' || playAgain == 'y') {
-          if (!hasGuess) {
+          isCharacter = true;
+          while (isCharacter) {
                guessing();
           }
-          hasGuess = false;
 
-          while (guess != secretNumber)
-          {
-               if (guess > secretNumber) {
-                    toHigh();
-               }
-               else {
-                    toLow();
-               }
+
+          while (guess != secretNumber){
+          isCharacter = true;
+          while (isCharacter) {
+          if (guess > secretNumber) {
+               toHigh();
           }
+          else {
+               toLow();
+          }
+     }
+}
 
           pinPoint();
 
